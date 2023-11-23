7060e3a2b9f269097ae0ea9532de092be01adcd3
Rachit Dua <rachit2427@gmail.com>
2023-11-23 22:12:33 +0530

    Added rest hooks

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -3,13 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import type { ViewProps } from 'react-native/types';
 
 import { StatusBar } from 'expo-status-bar';
+import { CacheProvider } from '@rest-hooks/react';
 
 const AppEntryComponent: React.FC<ViewProps> = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <CacheProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </CacheProvider>
   );
 };
 
